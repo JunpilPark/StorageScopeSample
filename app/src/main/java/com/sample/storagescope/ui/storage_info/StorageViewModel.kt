2625a8d65384f2f68f8579af9bd63860f1ec50a2
junpil Park <iamfeel!2@gmail.com>
2022-02-07 00:17:31 +0900

    feat: 외부저장소 경로 표시

--- app/src/main/java/com/sample/storagescope/ui/storage_info/StorageViewModel.kt
+++ app/src/main/java/com/sample/storagescope/ui/storage_info/StorageViewModel.kt
@@ -2,8 +2,10 @@ package com.sample.storagescope.ui.storage_info
 
 import android.app.Application
 import android.os.Build
+import android.os.Environment
 import android.os.storage.StorageManager
 import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
 import androidx.core.content.getSystemService
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
@@ -21,10 +23,64 @@ class StorageViewModel(application: Application): AndroidViewModel(application) 
     val externalStoragePath: LiveData<String> get() = _externalStoragePaths
 
     init {
-        _internalStoragePaths.value = getInternalStorageSections(application)
-            .map { it.toString() }
-            .reduce { string1, string2 -> "${string1}\n${string2}" }
+        _internalStoragePaths.value = getInternalStorageSections(application).joinToString("\n") { it.toString() }
+        _externalStoragePaths.value = getExternalStorageSections(application).joinToString("\n") { it.toString() }
     }
+
+    private fun getExternalStorageSections(application: Application): List<Section> {
+        return listOf(
+            getExternalStorageAvailableSection(),
+            getExternalStoragePathSection(application),
+            getExternalCacheStoragePathSection(application)
+        )
+    }
+
+    // 읽기, 쓰기 모두 가능한 지 확인
+    private fun isExternalStorageWritable(): Boolean {
+        return Environment.getExternalStorageState() == Environment.MEDIA_MOUNTED
+    }
+
+    // 최소한 읽기는 가능한지 가능
+    private fun isExternalStorageReadable(): Boolean {
+        return Environment.getExternalStorageState() in
+                setOf(Environment.MEDIA_MOUNTED, Environment.MEDIA_MOUNTED_READ_ONLY)
+    }
+
+    private fun getExternalStorageAvailableSection(): Section {
+        val paragraph = Paragraph(
+            "외부저장소 마운트 여부",
+            when {
+                isExternalStorageWritable() -> "정상 마운트 됨, 읽기/쓰기 가능"
+                isExternalStorageReadable() -> "정상 마운트 됨, 읽기 가능"
+                else -> "마우트 안됨, 외부저장소 사용 불가"
+            })
+        return Section().apply {
+            append(paragraph)
+        }
+    }
+
+    private fun getExternalStoragePathSection(application: Application): Section {
+        val externalStorageVolumes: Array<File> = ContextCompat.getExternalFilesDirs(application, null) // 외부저장소
+        val paragraph = Paragraph(
+            "외부 저장소 위치 (index 0 default)",
+            externalStorageVolumes.mapIndexed { index, file -> "[$index] ${file.path}" }.joinToString("\n")
+        )
+        return Section().apply {
+            append(paragraph)
+        }
+    }
+
+    private fun getExternalCacheStoragePathSection(application: Application): Section {
+        val externalStorageVolumes: Array<File> = ContextCompat.getExternalCacheDirs(application) // 외부저장소
+        val paragraph = Paragraph(
+            "외부 저장소의 캐시 디렉토리 위치 (index 0 default)",
+            externalStorageVolumes.mapIndexed { index, file -> "[$index] ${file.path}" }.joinToString("\n")
+        )
+        return Section().apply {
+            append(paragraph)
+        }
+    }
+
 
     private fun getInternalStorageSections(application: Application) = listOf(
         getFilesDirDirInInternalStorageSection(application),
@@ -46,7 +102,7 @@ class StorageViewModel(application: Application): AndroidViewModel(application) 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             section.append(Paragraph("filesDir 의 사용 가능한 용량", getAvailableBytes(application.filesDir).toShortStorageUnit()))
         }
-        return  section.append(Paragraph("filesDir.absolutePath", application.filesDir.absolutePath))
+        return  section.append(Paragraph("filesDir.absolutePath", application.filesDir.absolutePath)) // 내부저장소
             .append(Paragraph("filesDir.path", application.filesDir.path))
     }
 
